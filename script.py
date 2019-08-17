cd6b8d5268a7baf45db61d757f1e2110df8dfdc7
Anja <sanjanirina@gmail.com>
2019-08-17 16:25:46 +0300

    Mon app Django Girls, premier commit

---
+++ script.py
@@ -0,0 +1,14 @@
+print ("salut Anja")
+age=24
+if (age < 30 and age > 18):
+	print("vous ete jeune")
+elif (age < 45 and age > 31):
+	print("vous ete adulte")
+elif (age < 70 and age > 46):
+	print("vous ete vieux")
+def bonjour(name):
+	print("bonjour " + name + " ça va")
+
+bonjour("Anja")
+bonjour("Antsa")
+bonjour("Ony")
